9691478742e5ee6ef03ad80abdeb7415d16cf568
Adam Lindberg <hello@alind.io>
2017-06-21 14:54:39 +0200

    Add GPIO gen_event manager

--- src/grisp_sup.erl
+++ src/grisp_sup.erl
@@ -19,13 +19,12 @@ init([]) ->
     Drivers = application:get_env(grisp, drivers, []),
     SPIDriver = proplists:get_value(spi, Drivers, grisp_spi_drv),
     GPIODriver = proplists:get_value(gpio, Drivers, grisp_gpio_drv),
+    % FIXME: Erlang 19+: #{id => ID, start => {M, F, A}},
     {ok, {{one_for_one, 1, 5}, [
-        % FIXME: Erlang 19+: #{id => spi, start => {grisp_spi, start_link, []}},
-        {spi, {grisp_spi, start_link, [SPIDriver]}, permanent, infinity, worker, [grisp_spi, grisp_spi_drv]},
-        % FIXME: Erlang 19+: #{id => gpio, start => {grisp_gpio, start_link, []}},
-        {gpio, {grisp_gpio, start_link, [GPIODriver]}, permanent, infinity, worker, [grisp_gpio, grisp_gpio_drv]},
-        % FIXME: Erlang 19+: #{id => led, start => {grisp_led, start_link, []}},
-        {led, {grisp_led, start_link, []}, permanent, infinity, worker, [grisp_led]},
-        % FIXME: Erlang 19+: #{id => devices, start => {grisp_device_sup, start_link, []}, type => supervisor}
-        {devices, {grisp_device_sup, start_link, []}, permanent, infinity, supervisor, [grisp_device_sup]}
+        {grisp_spi, {grisp_spi, start_link, [SPIDriver]}, permanent, infinity, worker, [grisp_spi, grisp_spi_drv]},
+        {grisp_gpio, {grisp_gpio, start_link, [GPIODriver]}, permanent, infinity, worker, [grisp_gpio, grisp_gpio_drv]},
+        {grisp_gpio_events, {gen_event, start_link, [{local, grisp_gpio_events}]}, permanent, infinity, worker, [gen_event]},
+        {grisp_gpio_poller, {grisp_gpio_poller, start_link, []}, permanent, infinity, worker, [grisp_gpio_poller]},
+        {grisp_led, {grisp_led, start_link, []}, permanent, infinity, worker, [grisp_led]},
+        {grisp_devices, {grisp_device_sup, start_link, []}, permanent, infinity, supervisor, [grisp_device_sup]}
     ]}}.
