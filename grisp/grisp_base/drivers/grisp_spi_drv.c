5e9acb68298eff3babf2ba40d8a421351588e47d
Adam Lindberg <hello@alind.io>
2017-06-21 15:15:34 +0200

    Add OTP main code and drivers

---
+++ grisp/grisp_base/drivers/grisp_spi_drv.c
@@ -0,0 +1,145 @@
+/* spi_drv.c */
+
+#include <assert.h>
+#include <fcntl.h>
+#include <rtems.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <sys/ioctl.h>
+
+#include <bsp.h>
+#include <bsp/atsam-spi.h>
+#include <bsp/spi.h>
+#include <dev/spi/spi.h>
+
+#include "erl_driver.h"
+#include "sys.h"
+
+
+int grisp_spi_init (void);
+ErlDrvData grisp_spi_start (ErlDrvPort port, char *command);
+void grisp_spi_stop (ErlDrvData drv_data);
+void grisp_spi_output (ErlDrvData drv_data, char *buf, ErlDrvSizeT len);
+
+ErlDrvEntry grisp_spi_driver_entry = {
+    grisp_spi_init,
+    grisp_spi_start,
+    grisp_spi_stop,
+    grisp_spi_output,
+    NULL,
+    NULL,
+    "grisp_spi_drv",
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    ERL_DRV_EXTENDED_MARKER,
+    ERL_DRV_EXTENDED_MAJOR_VERSION,
+    ERL_DRV_EXTENDED_MINOR_VERSION,
+    0,
+    NULL,
+    NULL,
+    NULL
+};
+
+struct grisp_spi_data {
+    ErlDrvPort port;
+    char cnt;
+    int fd;
+};
+
+/* FIXME: consider making this more dynamic, especially if generalized */
+static struct grisp_spi_data grisp_spi_data = { NULL, 0, -1 };
+
+/* Make sure to keep this at sync with the -define(res_max_size.. in spi.erl */
+#define RES_MAX_SIZE 256
+
+int grisp_spi_init (void)
+{
+    int rv;
+
+    /* bus registration */
+    rv = spi_bus_register_atsam(ATSAM_SPI_0_BUS_PATH, ID_SPI0, SPI0, NULL, 0);
+    assert(rv == 0);
+    return 0;
+}
+
+ErlDrvData grisp_spi_start (ErlDrvPort port, char *command)
+{
+    int rv;
+    uint32_t speed = 100000;
+
+    if (grisp_spi_data.port != NULL)
+        return ERL_DRV_ERROR_GENERAL;
+
+    grisp_spi_data.port = port;
+    grisp_spi_data.cnt = 1;
+
+    grisp_spi_data.fd = open(ATSAM_SPI_0_BUS_PATH, O_RDWR);
+    assert(grisp_spi_data.fd != -1);
+
+    rv = ioctl(grisp_spi_data.fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
+    assert(rv == 0);
+
+    return (ErlDrvData)&grisp_spi_data;
+}
+
+void grisp_spi_stop (ErlDrvData drv_data)
+{
+    ASSERT ((struct grisp_spi_data *)drv_data == &grisp_spi_data);
+    close(grisp_spi_data.fd);
+    grisp_spi_data.port = NULL;
+}
+
+static void
+test_set_default_msg(struct spi_ioc_transfer *msg)
+{
+	msg->rx_buf = NULL;
+	msg->tx_buf = NULL;
+	msg->len = 0;
+	msg->speed_hz = 100000;
+	msg->delay_usecs = 1;
+	msg->bits_per_word = 8;
+	msg->cs_change = 1;
+	msg->rx_nbits = 0;
+	msg->tx_nbits = 0;
+	msg->mode = 0;
+	msg->cs = 2;
+}
+
+void grisp_spi_output (ErlDrvData drv_data, char *buf, ErlDrvSizeT len)
+{
+    int rv;
+    uint cs;
+    char res[RES_MAX_SIZE];
+    struct spi_ioc_transfer msg;
+
+    // Grab first byte as chip select, and shorten buffer by 1
+    cs = buf[0];
+    buf++;
+    len -= 1;
+
+    ASSERT ((struct grisp_spi_data *)drv_data == &grisp_spi_data);
+    ASSERT (grisp_spi_data.port != NULL);
+
+    grisp_spi_data.cnt++;
+
+    assert(len <= RES_MAX_SIZE);
+    test_set_default_msg(&msg);
+    msg.cs = cs;
+    msg.tx_buf = buf;
+    msg.rx_buf = res;
+    msg.len = len;
+
+    rv = ioctl(grisp_spi_data.fd, SPI_IOC_MESSAGE(1), &msg);
+    assert(rv == 0);
+
+    driver_output(grisp_spi_data.port, res, len);
+}
---
+++ grisp/grisp_base/sys/erl_main.c
@@ -0,0 +1,389 @@
+// #define UID_ERL_LOAD_NFS
+
+/*
+ * %CopyrightBegin%
+ * 
+ * Copyright Ericsson AB 2000-2009. All Rights Reserved.
+ * 
+ * The contents of this file are subject to the Erlang Public License,
+ * Version 1.1, (the "License"); you may not use this file except in
+ * compliance with the License. You should have received a copy of the
+ * Erlang Public License along with this software. If not, it can be
+ * retrieved online at http://www.erlang.org/.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * %CopyrightEnd%
+ */
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+#include "sys.h"
+#include "erl_vm.h"
+#include "global.h"
+
+#ifdef __rtems__
+#include <rtems.h>
+#include <rtems/shell.h>
+#include <rtems/console.h>
+#include <rtems/malloc.h>
+#include <machine/rtems-bsd-commands.h>
+#include <bsp.h>
+#include <assert.h>
+#include <rtems/libio.h>
+
+#include <inih/ini.h>
+
+#include <grisp/pin-config.h>
+#include <grisp/led.h>
+#include <grisp/init.h>
+
+#define MNT "/media/mmcsd-0-0/"
+#define INI_FILE (MNT "grisp.ini")
+
+void parse_args(char *args);
+
+const Pin atsam_pin_config[] = {GRISP_PIN_CONFIG};
+const size_t atsam_pin_config_count = PIO_LISTSIZE(atsam_pin_config);
+const uint32_t atsam_matrix_ccfg_sysio = GRISP_MATRIX_CCFG_SYSIO;
+
+static int start_dhcp = 0;
+static int wlan_enable = 0;
+
+static char *erl_args = "erl.rtems -- -root /media/mmcsd-0-0/otp"
+    " -home /media/mmcsd-0-0/home -boot start_sasl -pa /media/mmcsd-0-0/";
+
+#define MAX_ARGC 256
+
+static char *argv[MAX_ARGC];
+static int argc;
+
+void 
+fatal_extension(uint32_t source, uint32_t is_internal, uint32_t error)
+{
+  printk ("fatal extension: source=%ld, is_internal=%ld, error=%ld\n",
+	  source, is_internal, error);
+  if (source == RTEMS_FATAL_SOURCE_EXCEPTION)
+    rtems_exception_frame_print((const rtems_exception_frame *)error);
+      
+  /* rtems_stack_checker_report_usage(); */
+  while(1)
+    {
+    }
+}
+
+void
+fatal_atexit(void)
+{
+  printk ("Erlang VM exited\n");
+  /* rtems_stack_checker_report_usage(); */
+  while(1)
+    {
+    }
+}
+
+static int ini_file_handler(void *arg, const char *section, const char *name, 
+			    const char *value)
+{
+  int ok = 0;
+
+  printf ("grisp.ini: "
+	  "section \"%s\", name \"%s\", value \"%s\"\n",
+	  section, name, value);
+  if (strcmp(section, "network") == 0) {
+      if (strcmp(name, "ip_self") == 0) {
+	  if (strcmp(value, "dhcp") == 0) {
+	      start_dhcp = 1;
+	      ok = 1;
+	  }
+      }
+      else if (strcmp(name, "wlan") == 0) {
+	  if (strcmp(value, "enable") == 0) {
+	      wlan_enable = 1;
+	      ok = 1;
+	  }
+	  else if (strcmp(value, "disable") == 0) {
+	      wlan_enable = 0;
+	      ok = 1;
+	  }
+      }
+  }
+  else if (strcmp(section, "erlang") == 0) {
+      if (strcmp(name, "args") == 0) {
+	  printf ("erl args: "
+		  "section \"%s\", name \"%s\", value \"%s\"\n",
+		  section, name, value);
+	  erl_args = strdup(value);
+	  ok = 1;
+      }
+  }
+  else
+    ok = 1;
+
+  if (!ok) {
+      printf ("erl_main: error in configuration file: "
+	      "section \"%s\", name \"%s\", value \"%s\"\n",
+	      section, name, value);
+      ok = 1;
+    }
+
+  return ok;
+}
+
+static void evaluate_ini_file(const char *ini_file)
+{
+    int rv;
+    
+    rv = ini_parse(ini_file, ini_file_handler, NULL);
+    if (rv == -1) {
+	printf("WARNING: Can't find ini file %s -> using defaults\n", ini_file);
+    }
+}
+
+static void
+network_dhcpcd_task(rtems_task_argument arg)
+{
+	int exit_code;
+	char *dhcpcd[] = {
+		"dhcpcd",
+		NULL
+	};
+
+	(void)arg;
+
+	exit_code = rtems_bsd_command_dhcpcd(RTEMS_BSD_ARGC(dhcpcd), dhcpcd);
+	assert(exit_code == EXIT_SUCCESS);
+}
+
+static void
+start_network_dhcpcd(void)
+{
+	rtems_status_code sc;
+	rtems_id id;
+
+	sc = rtems_task_create(
+		rtems_build_name('D', 'H', 'C', 'P'),
+		RTEMS_MAXIMUM_PRIORITY - 1,
+		2 * RTEMS_MINIMUM_STACK_SIZE,
+		RTEMS_DEFAULT_MODES,
+		RTEMS_FLOATING_POINT,
+		&id
+	);
+	assert(sc == RTEMS_SUCCESSFUL);
+
+	sc = rtems_task_start(id, network_dhcpcd_task, 0);
+	assert(sc == RTEMS_SUCCESSFUL);
+}
+
+static void
+create_wlandev(void)
+{
+	int exit_code;
+	char *ifcfg[] = {
+		"ifconfig",
+		"wlan0",
+		"create",
+		"wlandev",
+		"rtwn0",
+		"up",
+		NULL
+	};
+
+	exit_code = rtems_bsd_command_ifconfig(RTEMS_BSD_ARGC(ifcfg), ifcfg);
+	if(exit_code != EXIT_SUCCESS) {
+		printf("ERROR while creating wlan0.");
+	}
+}
+
+void parse_args(char *args)
+{
+    char *p;
+    char *last;
+
+    for (p = strtok_r(args, " \t", &last);
+	 p;
+	 p = strtok_r(NULL, " \t", &last))
+    {
+	if (argc >= MAX_ARGC) {
+	        printf("ERROR: too many erl arguments\n");
+		exit(-1);
+	}
+		
+	argv[argc++] = p;
+    }
+}
+
+static void Init(rtems_task_argument arg)
+{
+#if 0
+    char *argv[] = { "erl.rtems", /* "-vsgMatpmX", */ "--", "-root", MNT "otp",
+		   "-home", MNT "home", "-boot", "start_sasl",
+		   "-pa", MNT
+		   /* "-noshell", "-noinput", */
+		   /* "-config", "/mnt/uid", */
+		   /* "-internal_epmd", "epmd_sup", "-sname", "uid" */
+		   /* "-init_debug", "-loader_debug" */
+  };
+
+  int argc = sizeof(argv)/sizeof(*argv);
+#endif
+  rtems_status_code sc = RTEMS_SUCCESSFUL;
+  int rv = 0;
+
+  printf("\nerl_main: starting ...\n");
+  atexit(fatal_atexit);
+  
+  grisp_led_set1(false, false, false);
+  grisp_led_set2(true, true, true);
+  printf("mounting sd card\n");
+  grisp_init_sd_card();
+  grisp_init_lower_self_prio();
+  grisp_init_libbsd();
+  
+  /* Wait for the SD card */
+  grisp_led_set2(true, false, true);
+  sc = grisp_init_wait_for_sd();
+  if(sc == RTEMS_SUCCESSFUL) {
+    printf("sd card mounted\n");
+  } else {
+    printf("ERROR: SD could not be mounted after timeout\n");
+    grisp_led_set2(true, false, false);
+  }
+
+  evaluate_ini_file(INI_FILE);
+  printf("%s\n", erl_args);
+  parse_args(erl_args);
+
+  if(start_dhcp) {
+      grisp_led_set2(false, true, true);
+      start_network_dhcpcd();
+  }
+  if (wlan_enable) {
+      grisp_led_set2(false, false, true);
+      rtems_task_wake_after(RTEMS_MILLISECONDS_TO_TICKS(4000));
+      create_wlandev();
+  }
+  grisp_led_set2(false, true, false);
+
+  printf("mkdir /tmp\n");
+  rv = mkdir("/tmp", 0755);
+  assert(rv == 0);
+
+  printf("mkdir /tmp/log\n");
+  rv = mkdir("/tmp/log", 0755);
+  assert(rv == 0);
+
+  printf("mkdir /home\n");
+  rv = mkdir("/home", 0755);
+  assert(rv == 0);
+
+  printf("Setting environment\n");
+  setenv("BINDIR", MNT "otp/lib/erlang/bin", 1);
+  setenv("ROOTDIR", MNT "otp", 1);
+  setenv("PROGNAME", "erl.rtems", 1);
+  /* setenv("ERL_INETRC", MNT "home/erl_inetrc", 1); */
+  /* setenv("ERL_LIBS", MNT "apps", 1); */
+  setenv("HOME", MNT "home", 1);
+
+  printf("starting erlang runtime\n");
+  erl_start(argc, argv);
+  printf("erlang runtime exited\n");
+  sleep(2);
+  exit(0);
+}
+
+#define FATAL_EXTENSION			      \
+  {					      \
+      NULL,				      \
+      NULL,				      \
+      NULL,				      \
+      NULL,				      \
+      NULL,				      \
+      NULL,				      \
+      NULL,				      \
+      fatal_extension			      \
+   }
+
+/*
+ * Configure LibBSD.
+ */
+#define RTEMS_BSD_CONFIG_BSP_CONFIG
+#define RTEMS_BSD_CONFIG_INIT
+#define RTEMS_BSD_CONFIG_TERMIOS_KQUEUE_AND_POLL
+
+#include <machine/rtems-bsd-config.h>
+
+#define CONFIGURE_MICROSECONDS_PER_TICK 1000
+
+#define CONFIGURE_APPLICATION_NEEDS_CLOCK_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_STUB_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_ZERO_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_LIBBLOCK
+
+#define CONFIGURE_USE_IMFS_AS_BASE_FILESYSTEM
+#define CONFIGURE_FILESYSTEM_DOSFS
+
+/* increase max file size in IMFS to 64MB */
+#define CONFIGURE_IMFS_MEMFILE_BYTES_PER_BLOCK 256 
+
+#define CONFIGURE_FILESYSTEM_IMFS
+
+#define CONFIGURE_LIBIO_MAXIMUM_FILE_DESCRIPTORS 64
+
+#define CONFIGURE_MAXIMUM_TASKS 64
+#define CONFIGURE_MAXIMUM_DRIVERS 8
+#define CONFIGURE_MAXIMUM_SEMAPHORES 32
+#define CONFIGURE_MAXIMUM_MESSAGE_QUEUES 4
+#define CONFIGURE_MAXIMUM_TIMERS 8
+#define CONFIGURE_MAXIMUM_PERIODS 4
+
+#define CONFIGURE_MAXIMUM_POSIX_THREADS 4
+#define CONFIGURE_MAXIMUM_POSIX_MUTEXES 16
+
+#define CONFIGURE_EXTRA_TASK_STACKS (512 * 1024)
+
+#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
+
+#define CONFIGURE_INIT_TASK_STACK_SIZE (512 * 1024)
+
+#define CONFIGURE_IDLE_TASK_STACK_SIZE (512 * 1024)
+
+/* #define CONFIGURE_STACK_CHECKER_ENABLED */
+
+#define CONFIGURE_UNLIMITED_ALLOCATION_SIZE 8
+#define CONFIGURE_MAXIMUM_POSIX_KEYS      16
+
+#define CONFIGURE_UNLIMITED_OBJECTS
+#define CONFIGURE_UNIFIED_WORK_AREAS
+#define CONFIGURE_MAXIMUM_USER_EXTENSIONS 8
+
+#define CONFIGURE_INITIAL_EXTENSIONS      FATAL_EXTENSION
+
+
+#define CONFIGURE_BDBUF_BUFFER_MAX_SIZE (16 * 1024)
+#define CONFIGURE_BDBUF_MAX_READ_AHEAD_BLOCKS 4
+#define CONFIGURE_BDBUF_CACHE_MEMORY_SIZE (1 * 1024 * 1024)
+
+#define CONFIGURE_PIPES_ENABLED
+#define CONFIGURE_MAXIMUM_PIPES 16
+
+#define CONFIGURE_INIT
+
+#define CONFIGURE_MALLOC_DIRTY
+
+#define CONFIGURE_INIT_TASK_PRIORITY 10
+
+#include <rtems/confdefs.h>
+
+#else
+  int
+    main(int argc, char **argv)
+  {
+    erl_start(argc, argv);
+    return 0;
+  }
+#endif
