a1bf84370670ab02cfcf04e58d898bc45d8ff050
Adam Lindberg <hello@alind.io>
2017-07-12 16:39:47 +0200

    Simplify device runtime data to only one ETS table

--- src/grisp_device.erl
+++ src/grisp_device.erl
@@ -11,45 +11,35 @@
 %--- API -----------------------------------------------------------------------
 
 slot(Slot) ->
-    case ets:lookup(grisp_devices, Slot) of
+    case ets:lookup(?MODULE, Slot) of
         [] -> error({no_device_connected, Slot});
         [Device] -> Device
     end.
 
 default(Driver) ->
-    % TODO: This means 2 ETS lookups for every default driver call, optimize?
-    case ets:lookup(grisp_devices_default, Driver) of
-        []               -> error({no_device_present, Driver});
-        [{Driver, Slot}] -> slot(Slot)
+    case ets:match_object(?MODULE, #device{driver = Driver, _ = '_'}, 1) of
+        {[], _}       -> error({no_device_present, Driver});
+        {[Device], _} -> Device
     end.
 
 setup(Configuration) ->
-    ets:new(grisp_devices, [
+    ets:new(?MODULE, [
         named_table,
+        ordered_set,
         {keypos, #device.slot},
-        {read_concurrency, true}
-    ]),
-    ets:new(grisp_devices_default, [
-        named_table,
         {read_concurrency, true}
     ]),
 
     % TODO: Validate ports
-    % FIXME: Use maps for devices in later Erlang versions
-    Default = lists:foldl(fun({Slot, Driver}, Default) ->
-        Pid = grisp_device_sup:start_child(Slot, Driver),
-        Device = #device{slot = Slot, driver = Driver, instance = Pid},
-        ets:insert(grisp_devices, Device),
-        % FIXME: 19+: maps:update_with(Driver, fun(V) -> V end, Device, Default)
-        case proplists:is_defined(Driver, Default) of
-            true  -> Default;
-            false -> [{Driver, Slot}|Default]
-        end
-    end, [], Configuration),
-
-    [ets:insert(grisp_devices_default, D) || D <- Default],
-
-    [grisp_devices, grisp_devices_default].
+    [init_device(Slot, Driver) || {Slot, Driver} <- Configuration],
+    [grisp_devices].
 
 teardown(Tables) ->
     [ets:delete(T) || T <- Tables].
+
+%--- Internal ------------------------------------------------------------------
+
+init_device(Slot, Driver) ->
+    Pid = grisp_device_sup:start_child(Slot, Driver),
+    Device = #device{slot = Slot, driver = Driver, instance = Pid},
+    ets:insert(?MODULE, Device).
