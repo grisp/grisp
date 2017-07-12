acf742fe1bce94e8a50f3aa34b9cda97e41b150d
Adam Lindberg <hello@alind.io>
2017-07-12 16:19:50 +0200

    Add 1-wire placeholder

---
+++ src/grisp_onewire.erl
@@ -0,0 +1,10 @@
+-module(grisp_onewire).
+
+% API
+-export([do_stuff/1]).
+
+%--- API -----------------------------------------------------------------------
+
+% FIXME: Placeholder API
+do_stuff(Things) ->
+    grisp_i2c:do_stuff(Things).
