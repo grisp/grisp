5f6031166c21505b5a80bda38d674e74c22ec63d
Adam Lindberg <hello@alind.io>
2017-07-12 16:16:58 +0200

    Streamline driver selection

--- src/grisp_sup.erl
+++ src/grisp_sup.erl
@@ -16,17 +16,14 @@ start_link() ->
 %--- Callbacks -----------------------------------------------------------------
 
 init([]) ->
-    Drivers = application:get_env(grisp, drivers, []),
-    SPIDriver = proplists:get_value(spi, Drivers, grisp_spi_drv),
-    GPIODriver = proplists:get_value(gpio, Drivers, grisp_gpio_drv),
     SupFlags = #{
         strategy => one_for_one,
         intensity => 1,
         period => 5
     },
     Children = [
-        worker(grisp_spi, [SPIDriver]),
-        worker(grisp_gpio, [GPIODriver]),
+        worker(grisp_spi, [driver(spi, grisp_spi_drv)]),
+        worker(grisp_gpio, [driver(gpio, grisp_gpio_drv)]),
         worker(grisp_gpio_events, gen_event, [{local, grisp_gpio_events}]),
         worker(grisp_gpio_poller, []),
         worker(grisp_led, []),
@@ -41,3 +38,7 @@ worker(ID, M, A) -> #{id => ID, start => {M, start_link, A}}.
 
 supervisor(ID, A) ->
     #{id => ID, start => {ID, start_link, A}, type => supervisor}.
+
+driver(Driver, DefaultMod) ->
+    Drivers = application:get_env(grisp, drivers, []),
+    proplists:get_value(Driver, Drivers, DefaultMod).
