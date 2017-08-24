b75ae2b89a26e0462ff89e797bca24bc549fdcaa
Adam Lindberg <hello@alind.io>
2017-08-24 15:05:26 +0200

    Add grisp_led documentation

--- src/grisp_led.erl
+++ src/grisp_led.erl
@@ -17,6 +17,36 @@
 -export([code_change/3]).
 -export([terminate/2]).
 
+%--- Types ---------------------------------------------------------------------
+
+-type position() :: 1 | 2.
+% The position of the LED on the GRiSP board, either `1' or `2'.
+
+-type color() :: color_name() | color_value().
+% A color as either a shorthand name or a specific RGB value.
+
+-type color_name() ::
+    black |
+    blue |
+    green |
+    aqua |
+    red |
+    magenta |
+    yellow |
+    white.
+% A color name.
+
+-type color_value() :: {0 | 1, 0 | 1, 0 | 1}.
+% A color value, specifying the individual R, G and B components as 1's or 0's
+% where 1 means on and 0 means off.
+
+-type time() :: pos_integer() | infinity.
+% A time interval for a color in milliseconds. Must be 1 or above, or
+% alternatively `infinity'.
+
+-type pattern() :: [{time(), color()}].
+% A list of intervals and colors to show during those intervals.
+
 %--- Records -------------------------------------------------------------------
 
 -record(state, {driver, leds}).
@@ -31,13 +61,54 @@
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, undefined, []).
 
+% @doc Set the color of an LED.
+%
+% <h5>Examples</h5>
+% ```
+% 1> grisp_led:color(1, red)
+% ok
+% 2> grisp_led:color(2, {0, 1, 0})
+% ok
+% '''
+-spec color(position(), color()) -> ok.
 color(Pos, Color) -> pattern(Pos, [{infinity, Color}]).
 
+% @doc Turn of an LED.
+% @equiv grisp_led:color(Pos, black)
+-spec off(position()) -> ok.
 off(Pos) -> pattern(Pos, [{infinity, black}]).
 
+% @doc Flash an LED in an on/off pattern with the specified color.
+%
+% <h5>Examples</h5>
+% ```
+% 1> grisp_led:flash(2, blue, 500).
+% ok
+% '''
+%
+% @equiv grisp_led:pattern(Position, [{Time, Color}, {Time, black}])
+-spec flash(position(), color(), time()) -> ok.
 flash(Pos, Color, Interval) ->
     pattern(Pos, [{Interval, Color}, {Interval, black}]).
 
+% @doc Animate an LED with a pattern of colors and intervals.
+%
+% <h5>Examples</h5>
+% ```
+% 1> grisp_led:flash(1, [{300, red}, {500, green}, {700, blue}, {infinity, black}]).
+% ok
+% 2> Rainbow = [{300, {R, G, B}} || R <- [0,1], G <- [0,1], B <- [0,1], {R, G, B} =/= {0, 0, 0}].
+% [{300,{0,0,1}},
+%  {300,{0,1,0}},
+%  {300,{0,1,1}},
+%  {300,{1,0,0}},
+%  {300,{1,0,1}},
+%  {300,{1,1,0}},
+%  {300,{1,1,1}}]
+% 3> grisp_led:flash(2, Rainbow).
+% ok
+% '''
+-spec pattern(position(), pattern()) -> ok.
 pattern(Pos, Pattern) -> gen_server:cast(?MODULE, {pattern, Pos, Pattern}).
 
 %--- Callbacks -----------------------------------------------------------------
