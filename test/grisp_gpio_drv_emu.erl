e079985c38fd8448eeb1c052aa9beb027ca28553
Adam Lindberg <hello@alind.io>
2017-08-30 18:21:34 +0200

    Reverse map emulated GPIO interface

--- test/grisp_gpio_drv_emu.erl
+++ test/grisp_gpio_drv_emu.erl
@@ -3,36 +3,56 @@
 % API
 -export([open/0]).
 -export([command/2]).
--export([led/3]).  %% for access in mocking
+-export([led/2]).  %% for access in mocking
 
 %--- API -----------------------------------------------------------------------
 
 open() -> undefined.
 
-command(_State, <<8, 3>>)  -> ok = led(1, red, off);
-command(_State, <<8, 4>>)  -> ok = led(1, red, on);
-command(_State, <<9, 3>>)  -> ok = led(1, green, off);
-command(_State, <<9, 4>>)  -> ok = led(1, green, on);
-command(_State, <<10, 3>>) -> ok = led(1, blue, off);
-command(_State, <<10, 4>>) -> ok = led(1, blue, on);
-command(_State, <<11, 3>>) -> ok = led(2, red, off);
-command(_State, <<11, 4>>) -> ok = led(2, red, on);
-command(_State, <<12, 3>>) -> ok = led(2, green, off);
-command(_State, <<12, 4>>) -> ok = led(2, green, on);
-command(_State, <<13, 3>>) -> ok = led(2, blue, off);
-command(_State, <<13, 4>>) -> ok = led(2, blue, on);
-command(_State, Command) ->
-    grisp_device_emu:broadcast({gpio, Command}).
+command(_State, <<Pin, Command>>) when Pin >= 8 andalso Pin =< 13 ->
+    led(index(Pin), command(Command));
+command(_State, <<Pin, Command>>) ->
+    grisp_device_emu:broadcast({gpio, index(Pin), command(Command)});
+command(_State, <<Pin, Command, Type, Attr>>) ->
+    grisp_device_emu:broadcast({gpio, index(Pin), {command(Command), type(Type), attr(Attr)}}).
 
+%--- Internal ------------------------------------------------------------------
 
-%% Add led function to enable mocking instead of visually observing
-%% We intercept calls to led instead of printing the output.
-led(Nr, Color, Status) ->
-  io:format("LED~p ~s ~s~n", [Nr, uppercase(Color), uppercase(Status)]).
+led(Color, Status) ->
+    io:format("~s ~s~n", [uppercase(Color), uppercase(Status)]).
 
-%% in R20 string:uppercase instead of string:to_upper
+%% TODO: In R20 use string:uppercase instead of string:to_upper
 uppercase(Atom) when is_atom(Atom) ->
-  uppercase(atom_to_list(Atom));
+    uppercase(atom_to_list(Atom));
 uppercase(String) ->
-  string:to_upper(String).
+    string:to_upper(String).
 
+index(8) -> led1_r;
+index(9) -> led1_g;
+index(10) -> led1_b;
+index(11) -> led2_r;
+index(12) -> led2_g;
+index(13) -> led2_b;
+index(14) -> jumper_1;
+index(15) -> jumper_2;
+index(16) -> jumper_3;
+index(17) -> jumper_4;
+index(18) -> jumper_5;
+index(19) -> spi1_pin9;
+index(20) -> spi1_pin10;
+index(21) -> ss1.
+
+type(0) -> periph_a;
+type(1) -> periph_b;
+type(2) -> periph_c;
+type(3) -> periph_d;
+type(4) -> input;
+type(5) -> output_0;
+type(6) -> output_1.
+
+attr(0) -> [default].
+
+command(1) -> configure;
+command(2) -> get;
+command(3) -> clear;
+command(4) -> set.
