7fbd28d9b7db66af4bf8d4c05c5297842efd37fb
Peer Stritzinger <peer@stritzinger.com>
2017-11-14 20:19:45 +0100

    Minimal Erlang termios just receiving linemod driver, maxsonar PMOD driver

---
+++ src/grisp_termios_drv.erl
@@ -0,0 +1,23 @@
+% @private
+-module(grisp_termios_drv).
+
+% API
+-export([open/0]).
+-export([command/2]).
+
+%--- Macros --------------------------------------------------------------------
+
+-define(PORT_COMMAND_TIMEOUT, 2000).
+
+%--- API -----------------------------------------------------------------------
+
+open() -> open_port({spawn_driver, "grisp_termios_drv"}, [binary]).
+
+command(Port, Command) ->
+    Port ! {self(), {command, <<Command/binary>>}},
+    receive
+        {Port, {data, Resp}} ->
+            Resp
+    after ?PORT_COMMAND_TIMEOUT ->
+            exit({i2c_driver_timeout, Command})
+    end.
