307c16a9189a0cba53de326ec77b0bd8aeed846f
nextloop <nextloop@i2pmail.org>
2018-02-06 00:34:41 +0100

    Docstrings and safeguards for grisp_ir_drv.erl

--- src/grisp_ir_drv.erl
+++ src/grisp_ir_drv.erl
@@ -21,26 +21,33 @@
 
 %--- API -----------------------------------------------------------------------
 
+%% @doc Opens up the port
 open() -> open_port({spawn_driver, "grisp_ir_drv"}, [binary]).
 
+%% @doc Registers the interrupt. It is not active until you enable it
 register_ir(Port, Pin, Attr) ->
     Command = <<(index(Pin)):8, 1:8, (map_type(input)):8, (map_attr(Attr)):8>>,
     command(Port, Command).
 
+%% @doc Activate interrupt. You will get an unlimited amount of messages
 activate_ir(Port, Pin) -> activate_ir(Port, Pin, 0).
 
-activate_ir(Port, Pin, N) when N >= 0 ->
+%% @doc Activate interrupt. You will get up to N messages. Do not rely on the exact number, you will most liekly detect less than N interrupts before you will have to activate it again.
+activate_ir(Port, Pin, N) when N >= 0, N =< 255 ->
     Command = <<(index(Pin)):8, 2:8, N:8>>,
     command(Port, Command).
 
+%% @doc Disables interrupt
 disable_ir(Port, Pin) ->
     Command = <<(index(Pin)), 3:8>>,
     command(Port, Command).
 
+%% @doc Removes interrupt
 remove_ir(Port, Pin) ->
     Command = <<(index(Pin)):8, 4:8>>,
     command(Port, Command).
 
+%% @doc Close the port
 close(Port) ->
     Port ! {self(), close},
     receive
@@ -49,7 +56,9 @@ close(Port) ->
 	    exit({ir_driver_timeout, close})
     end.
 
-command(Port, Command) ->
+%% Internal
+
+command(Port, Command) when is_port(Port), is_binary(Command) ->
     Port ! {self(), {command, Command}},
     receive
         {Port, {data, Resp}} ->
