e76c2d7e46f1c557666df888f33df5d4056dd409
Carlier <alexandre.carlier@student.uclouvain.be>
2018-08-05 11:36:58 +0200

    maxsonar driver version

--- src/pmod_maxsonar.erl
+++ src/pmod_maxsonar.erl
@@ -1,3 +1,4 @@
+
 -module(pmod_maxsonar).
 
 -behavior(gen_server).
@@ -16,7 +17,7 @@
 
 %--- Records -------------------------------------------------------------------
 
--record(state, {port, last_val}).
+-record(state, {port, list_val}).
 
 %--- API -----------------------------------------------------------------------
 
@@ -33,26 +34,45 @@ get() ->
 init(Slot = uart) ->
     Port = open_port({spawn_driver, "grisp_termios_drv"}, [binary]),
     grisp_devices:register(Slot, ?MODULE),
-    {ok, #state{port = Port}}.
+    {ok, #state{port = Port,list_val = []}}.
 
 % @private
-handle_call(get_value, _From, #state{last_val = Val} = State) ->
+handle_call(get_value, _From, #state{list_val = Val} = State) ->
     {reply, Val, State}.
+
+%handle_call({get_value,Req}, _From, #state{last_val = Val} = State) ->
+%  Data = grisp_termios:send_recv(Req),
+%  case Data of
+%      <<_, _, D1, D2, D3, 10>> when $0 =< D1, D1 =< $9,
+%                                    $0 =< D2, D2 =< $9,
+%                                    $0 =< D3, D3 =< $9 ->
+%          Val = (D1 - $0) * 100 + (D2 - $0) * 10 + (D3 - $0),
+%          {reply,Data, State#state{last_val = Val}};
+%      _ ->
+%          {reply,Data, State}
+%  end.
 
 % @private
 handle_cast(Request, _State) -> error({unknown_cast, Request}).
 
 % @private
-handle_info({Port, {data, Data}}, #state{port = Port} = State) ->
-    case Data of
-        <<_, _, D1, D2, D3, 10>> when $0 =< D1, D1 =< $9,
-                                      $0 =< D2, D2 =< $9,
-                                      $0 =< D3, D3 =< $9 ->
-            Val = (D1 - $0) * 100 + (D2 - $0) * 10 + (D3 - $0),
-            {noreply, State#state{last_val = Val}};
-        _ ->
-            {noreply, State}
-    end.
+handle_info({Port, {data, Data}}, #state{port = Port,list_val = Previous} = State) ->
+ if
+  byte_size(Data) > 5 -> NewList = Previous;
+  length(Previous) > 5 -> grisp_led:color(1,green), NewList = [];
+  byte_size(Data)==5 -> grisp_led:color(1,red),
+                        <<_:8,Val1:8,Val2:8,Val3:8,Rest>> = Data,
+                        List = [Val1,Val2,Val3],
+                        {NewValue,_} = string:to_integer(List),
+                        %  io:format("Value is: ~p ~n",[NewValue]),
+                        if
+                          NewValue < 50 -> NewList = lists:append([NewValue],Previous);
+                        true -> NewList = []
+                        end;
+  true -> NewList = Previous
+
+  end,
+  {noreply, State#state{list_val = NewList}}.
 
 
 
