5b09a90f7a58eef61fb36958bcbe8bc3ff300aad
Adam Lindberg <hello@alind.io>
2018-08-07 18:30:10 +0200

    Do not wait for termios response
    UART is asynchronous, no response is guaranteed. A device driver have to
    listen for data messages itself (if any).

--- src/grisp_termios_drv.erl
+++ src/grisp_termios_drv.erl
@@ -5,19 +5,10 @@
 -export([open/0]).
 -export([command/2]).
 
-%--- Macros --------------------------------------------------------------------
-
--define(PORT_COMMAND_TIMEOUT, 2000).
-
 %--- API -----------------------------------------------------------------------
 
 open() -> open_port({spawn_driver, "grisp_termios_drv"}, [binary]).
 
 command(Port, Command) ->
-    Port ! {self(), {command, <<Command/binary>>}},
-    receive
-        {Port, {data, Resp}} ->
-            Resp
-    after ?PORT_COMMAND_TIMEOUT ->
-            exit({i2c_driver_timeout, Command})
-    end.
+    port_command(Port, Command),
+    ok.
