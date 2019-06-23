e3ba70befbd8c490237016255a906eb6d577554d
Sebastian Bucki <sebastian.bucki@gmail.com>
2019-06-23 16:33:48 +0200

    Change hygro driver to gen_server

--- src/pmod_hygro.erl
+++ src/pmod_hygro.erl
@@ -1,29 +1,75 @@
 -module(pmod_hygro).
+-behaviour(gen_server).
 
 % API
--export([temp/0, humid/0, measurements/0]).
+-export([start_link/0]).
+-export([temp/0]).
+-export([humid/0]).
+-export([measurements/0]).
+
+% Callbacks
+-export([init/1]).
+-export([handle_call/3]).
+-export([handle_cast/2]).
+-export([handle_info/2]).
+-export([code_change/3]).
+-export([terminate/2]).
 
 -define(DEVICE_ADR,    16#40).
 -define(TEMP_REGISTER, 16#00).
 -define(DELAY_TIME, 15).
+%
+%--- Records -------------------------------------------------------------------
+%
+-record(state, {}).
 
 %--- API -----------------------------------------------------------------------
 
+% @private
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
 temp() ->
-    {ok, <<T:14/unsigned-big, _:2>>} = device_request(2),
-    Temp = evaluate_temp(T),
-    [{temp, Temp}].
+    gen_server:call(?MODULE, temp).
 
 humid() ->
-    {ok, <<_:14, _:2, H:14/unsigned-big, _:2>>} = device_request(4),
-    Humid = evaluate_humid(H),
-    [{humid, Humid}].
+    gen_server:call(?MODULE, humid).
 
 measurements() ->
+    gen_server:call(?MODULE, measurements).
+
+%--- Callbacks -----------------------------------------------------------------
+
+% @private
+init([]) ->
+  {ok, #state{}}.
+
+% @private
+handle_call(temp, _From, State) ->
+    {ok, <<T:14/unsigned-big, _:2>>} = device_request(2),
+    Temp = evaluate_temp(T),
+    {reply, [{temp, Temp}], State};
+handle_call(humid, _From, State) ->
+    {ok, <<_:14, _:2, H:14/unsigned-big, _:2>>} = device_request(4),
+    Humid = evaluate_humid(H),
+    {reply, [{humid, Humid}], State};
+handle_call(measurements, _From, State) ->
     {ok, <<T:14/unsigned-big, _:2, H:14/unsigned-big, _:2>>} = device_request(4),
     Temp = evaluate_temp(T),
     Humid = evaluate_humid(H),
-    [{temp, Temp}, {humid, Humid}].
+    {reply, [{temp, Temp}, {humid, Humid}], State}.
+
+% @private
+handle_cast(Request, _State) -> error({unknown_cast, Request}).
+
+% @private
+handle_info(Info, _State) -> error({unknown_info, Info}).
+
+% @private
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
+
+% @private
+terminate(_Reason, _State) -> ok.
 
 %--- Internal ------------------------------------------------------------------
 
