685c33cc579393b044c69717dfd2b8e303f4462a
Sebastien Merle <s.merle@gmail.com>
2019-07-31 13:02:01 +0200

    Add first draft of the NMEA parser

---
+++ test/grisp_nmea_tests.erl
@@ -0,0 +1,15 @@
+-module(grisp_nmea_tests).
+
+%--- Includes ------------------------------------------------------------------
+
+-include_lib("eunit/include/eunit.hrl").
+
+
+%--- Setup ---------------------------------------------------------------------
+
+gpgga_parsing_test() ->
+    ?assertMatch({ok, {gps, gga, #{time := 45319000, lat := 48.1173,
+                                   long := 11.516666666666667, alt := 545.4,
+                                   fixed := true}}},
+        grisp_nmea:parse(<<"$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47">>)),
+    ok.
