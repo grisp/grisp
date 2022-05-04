4821a1f4afbd779382b8ceda1c16f330ee45cc5a
Luca Succi <luca.succi@stritzinger.com>
2022-05-04 16:52:23 +0200

    new API to access second row of a pmod port

--- src/pmod_hb5.erl
+++ src/pmod_hb5.erl
@@ -2,17 +2,25 @@
 
 % API
 -export([open/1]).
+-export([open/2]).
 -export([stop/1]).
 -export([forward/1]).
 -export([backward/1]).
 
 %--- API -----------------------------------------------------------------------
-
 open(Slot) ->
+    open(Slot, first).
+open(Slot, first) ->
     Mode = #{mode => {output, 0}},
     {
         grisp_gpio:open(pin(Slot, 1), Mode),
         grisp_gpio:open(pin(Slot, 2), Mode)
+    };
+open(Slot, second) ->
+    Mode = #{mode => {output, 0}},
+    {
+        grisp_gpio:open(pin(Slot, 7), Mode),
+        grisp_gpio:open(pin(Slot, 8), Mode)
     }.
 
 stop({_Pin1, Pin2}) -> grisp_gpio:set(Pin2, 0).
