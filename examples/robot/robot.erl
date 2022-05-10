5398dee2ec445c6506e97b6bd3515c7d777546b5
Luca Succi <luca.succi@stritzinger.com>
2022-05-10 12:19:06 +0200

    example updated

--- examples/robot/robot.erl
+++ examples/robot/robot.erl
@@ -10,7 +10,7 @@
 %--- API -----------------------------------------------------------------------
 
 start() ->
-    State = {pmod_hb5:open(gpio1), pmod_hb5:open(gpio2)},
+    State = {pmod_hb5:open({gpio1,top}), pmod_hb5:open({gpio1,bottom})},
     commands(State, [{left, config}, {right, config}], 0).
 
 forward(State, Time) ->
