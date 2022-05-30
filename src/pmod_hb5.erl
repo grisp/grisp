af00c42d43532972add84494f5a38ae87d45e67f
Luca Succi <zioponi@gmail.com>
2022-05-30 12:12:21 +0200

    Export new UART and I2C pins, update pmod_hb5
    This changes the PmodHB5 API to allow access the different rows of a PMOD port.

--- src/pmod_hb5.erl
+++ src/pmod_hb5.erl
@@ -2,17 +2,27 @@
 
 % API
 -export([open/1]).
+-export([open/2]).
 -export([stop/1]).
 -export([forward/1]).
 -export([backward/1]).
 
 %--- API -----------------------------------------------------------------------
 
-open(Slot) ->
+open({gpio1, top}) ->
+    open(gpio1_1, gpio1_2);
+open({gpio1, bottom}) ->
+    open(gpio1_7, gpio1_8);
+open({uart, bottom}) ->
+    open(uart_7, uart_8);
+open(i2c) ->
+    open(i2c_1, i2c_2).
+
+open(Pin1, Pin2) ->
     Mode = #{mode => {output, 0}},
     {
-        grisp_gpio:open(pin(Slot, 1), Mode),
-        grisp_gpio:open(pin(Slot, 2), Mode)
+        grisp_gpio:open(Pin1, Mode),
+        grisp_gpio:open(Pin2, Mode)
     }.
 
 stop({_Pin1, Pin2}) -> grisp_gpio:set(Pin2, 0).
@@ -33,6 +43,3 @@ start({_Pin1, Pin2}) -> grisp_gpio:set(Pin2, 1).
 
 direction({Pin1, _Pin2}, forward)  -> grisp_gpio:set(Pin1, 0);
 direction({Pin1, _Pin2}, backward)  -> grisp_gpio:set(Pin1, 1).
-
-pin(Slot, Pin) ->
-    list_to_atom(atom_to_list(Slot) ++ "_" ++ integer_to_list(Pin)).
