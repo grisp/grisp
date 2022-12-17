e81be37c50076fd62762e02d8de15643a1bd831c
Adam Lindberg <hello@alind.io>
2022-12-17 10:18:21 +0100

    Support exclusive device access

--- src/grisp_internal.hrl
+++ src/grisp_internal.hrl
@@ -1,1 +1,11 @@
--record(device, {slot, driver, pid, monitor}).
+-record(device, {
+    slot,
+    driver,
+    driver_opts,
+    pid,
+    monitor,
+    access = public,
+    owner,
+    owner_monitor,
+    status = initializing}
+).
