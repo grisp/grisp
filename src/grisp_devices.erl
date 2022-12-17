15d8667cab2b21403405bf0282c2785112a8dfa2
Adam Lindberg <hello@alind.io>
2022-12-17 10:18:21 +0100

    Support exclusive device access

--- src/grisp_devices.erl
+++ src/grisp_devices.erl
@@ -10,6 +10,7 @@
 -export([remove_device/1]).
 -export([list/0]).
 -export([slot/1]).
+-export([slots/1]).
 -export([default/1]).
 -export([register/2]).
 
@@ -23,6 +24,8 @@
 
 -include("grisp.hrl").
 
+-define(DEFAULT_OPTS, #{access => public}).
+
 %--- API -----------------------------------------------------------------------
 
 start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, undefined, []).
@@ -33,30 +36,42 @@ setup() ->
     [apply(grisp, add_device, tuple_to_list(Dev)) || Dev <- Devices],
     ok.
 
-add_device(Slot, Driver, Opts) ->
-    grisp_devices_sup:start_child(Slot, Driver, Opts),
-    slot(Slot).
+add_device(Slot, Driver, UserOpts) ->
+    Opts = maps:merge(?DEFAULT_OPTS, UserOpts#{owner => self()}),
+    call({add, Slot, Driver, Opts}).
 
 remove_device(Device) ->
+    % TODO: Cleanup needed? Cancel monitors? Notify owner?
     grisp_devices_sup:terminate_child(Device).
 
 list() ->
     ets:tab2list(?MODULE).
 
 slot(Slot) ->
+    Self = self(),
     case ets:lookup(?MODULE, Slot) of
         [] -> error({no_device_connected, Slot});
-        [Device] -> Device
+        [#device{access = public} = Device] -> Device;
+        [#device{access = exclusive, owner = Self} = Device] -> Device;
+        [#device{owner = Owner}] -> error({protected, Owner})
     end.
 
+slots(Driver) ->
+    Pattern = #device{driver = Driver, slot = '$1', _ = '_'},
+    lists:flatten(ets:match(grisp_devices, Pattern)).
+
 default(Driver) ->
+    Self = self(),
     case ets:match_object(?MODULE, #device{driver = Driver, _ = '_'}, 1) of
-        {[Device], _} -> Device;
-        _             -> error({no_device_present, Driver})
+        [#device{access = public} = Device] -> Device;
+        [#device{owner = Self} = Device] -> Device;
+        [#device{owner = Owner}] -> error({protected, Owner});
+        [] -> error({no_device_present, Driver});
+        _ -> error(multiple_devices)
     end.
 
 register(Slot, Driver) ->
-    gen_server:call(?MODULE, {register, Slot, Driver, self()}).
+    call({register, Slot, Driver, self()}).
 
 %--- Callbacks -----------------------------------------------------------------
 
@@ -69,26 +84,61 @@ init(undefined) ->
     ]),
     {ok, undefined}.
 
+handle_call({add, Slot, Driver, Opts}, From, State) ->
+    #{owner := Owner, access := Access} = Opts,
+    DriverOpts = maps:without([owner, access], Opts),
+    spawn(fun() -> grisp_devices_sup:start_child(Slot, Driver, DriverOpts) end),
+    Device = #device{
+        slot = Slot,
+        driver = Driver,
+        driver_opts = DriverOpts,
+        access = Access,
+        owner = Owner,
+        owner_monitor = mon(Owner, owner),
+        status = {initializing, From}
+    },
+    case ets:insert_new(?MODULE, Device) of
+        true -> {noreply, State};
+        % TODO: Cleanup needed if device exists? Cancel monitors?
+        false -> error(not_implemented)
+    end;
 handle_call({register, Slot, Driver, Pid}, _From, State) ->
-    register_device(Slot, Driver, Pid),
-    {reply, ok, State}.
+    case ets:lookup(?MODULE, Slot) of
+        [#device{driver = Driver, status = {initializing, From}} = Device] ->
+            NewDevice = Device#device{
+                pid = Pid, monitor = mon(Pid, driver), status = ready
+            },
+            ets:insert(?MODULE, NewDevice),
+            gen_server:reply(From, NewDevice),
+            {reply, ok, State};
+        _ ->
+            {reply, {error, not_in_driver_process}, State}
+    end.
 
 handle_cast(Request, _State) -> error({unknown_cast, Request}).
 
-handle_info({'DOWN', Ref, process, Pid, _Reason}, State) ->
+handle_info({{'DOWN', driver}, Ref, process, Pid, _Reason}, State) ->
     ets:match_delete(?MODULE, #device{pid = Pid, monitor = Ref, _ = '_'}),
+    {noreply, State};
+handle_info({{'DOWN', owner}, Ref, process, Owner, _Reason}, State) ->
+    Pattern = #device{owner = Owner, owner_monitor = Ref, _ = '_'},
+    Devices = ets:match_object(?MODULE, Pattern),
+    [grisp_devices_sup:terminate_child(D) || D <- Devices],
+    ets:match_delete(?MODULE, Pattern),
     {noreply, State};
 handle_info(Info, _State) ->
     error({unknown_info, Info}).
 
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
 
-terminate(_Reason, _State) ->
-    ets:delete(?MODULE).
+terminate(_Reason, _State) -> ets:delete(?MODULE).
 
 %--- Internal ------------------------------------------------------------------
 
-register_device(Slot, Driver, Pid) ->
-    Ref = erlang:monitor(process, Pid),
-    Device = #device{slot = Slot, driver = Driver, pid = Pid, monitor = Ref},
-    ets:insert_new(?MODULE, Device).
+call(Call) ->
+    case gen_server:call(?MODULE, Call) of
+        {error, Error} -> error(Error);
+        Result -> Result
+    end.
+
+mon(Pid, Tag) -> erlang:monitor(process, Pid, [{tag, {'DOWN', Tag}}]).
