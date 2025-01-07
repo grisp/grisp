73766610b8ceb68c223c0a284432700a8eb1d016
Florian Odronitz <odo@mac.com>
2025-01-07 14:28:45 +0100

    Fixing unit in docs

--- src/grisp_pwm.erl
+++ src/grisp_pwm.erl
@@ -296,7 +296,7 @@ start_link(pwm, #{}) ->
 % ok
 % '''
 % === Clock, Prescale and Period Config Example ===
-% This sets a low frequency clock and results in a 500μs cycle time (2Hz) and 4bit resolution.
+% This sets a low frequency clock and results in a 500ms cycle time (2Hz) and 4bit resolution.
 % ```
 % 1> grisp_pwm:open(gpio1_8, {ipg_clk_32k, 1, <<16:16>>}, 0.5).
 % ok
